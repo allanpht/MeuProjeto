83393237b1446d06d0f534d27da64691dc40290c
allanpht <allan.lc@icloud.com>
2023-01-11 10:08:13 -0300

    Curso Angular - desenvolvedor.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './navegação/home/home.component';
+import { FooterComponent } from './navegação/footer/footer.component';
+import { MenuComponent } from './navegação/menu/menu.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouteConfig } from './app.routes';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    SobreComponent,
+    ContatoComponent,
+    DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    [RouterModule.forRoot(rootRouteConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
